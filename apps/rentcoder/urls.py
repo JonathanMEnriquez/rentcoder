0b7eb3702aa85828eb8c42e899d7974e82c725b5
user <user@users-MacBook-Pro.local>
2017-12-20 14:04:23 -0800

    inital commit

---
+++ apps/rentcoder/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^register$', views.register),
+    url(r'^home$', views.home),
+    url(r'^process/(?P<action>\w+)$', views.process),
+]
